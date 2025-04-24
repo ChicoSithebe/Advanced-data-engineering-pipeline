87277baa5224d45181d32cef5f0201de26a8b1cd
ChicoSithebe <u21717606@tuks.co.za>
2025-04-24 15:21:09 +0200

    Create table_ddl.sql

---
+++ sql/table_ddl.sql
@@ -0,0 +1,16 @@
+CREATE TABLE orders_cleaned (
+   Order_ID VARCHAR(20),
+   Order_Date DATE,
+   Ship_Date DATE,
+   Customer_ID VARCHAR(20),
+   Segment VARCHAR(30),
+   Country VARCHAR(50),
+   Region VARCHAR(30),
+   Product_ID VARCHAR(30),
+   Category VARCHAR(50),
+   Sales FLOAT,
+   Quantity INT,
+   Discount FLOAT,
+   Profit FLOAT,
+   Profit_Margin_PCT FLOAT
+);
